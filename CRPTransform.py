1351b4a616c5c710078debebdb2dfb78f964d49d
Daniel Johnson <danielrjohnson00@gmail.com>
2022-05-04 14:42:32 -0500

    added double xor

--- CRPTransform.py
+++ CRPTransform.py
@@ -8,6 +8,18 @@ def XorKeyTransform(challenges, key):
     return array([[ 1 if challenge[i] != arrayed_key[i] else -1 
         for i in range(len(arrayed_key))] for challenge in challenges]), key # return the xor
 
+def DoubleXorTransform(challenges, key):
+    arrayed_key = [-1 if bit == '0' else 1 for bit in bin(key)[2:]] # '0' -> -1, '1' -> 1, 0b cut off
+    first = arrayed_key[:len(arrayed_key)//2] * 2
+    second = arrayed_key[len(arrayed_key)//2:] * 2
+    assert len(first) == len(second) == len(challenges[0])
+
+    for k in [first, second]:
+        challenges = array([[ 1 if challenge[i] != k[i] else -1 
+                        for i in range(len(k))] for challenge in challenges])
+
+    return challenges, key # return the xor
+
 def TFFWithResetTransform(challenges, key):
     new_challenges = []
     for challenge in challenges:
--- __main__.py
+++ __main__.py
@@ -3,9 +3,9 @@ environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import matplotlib.pyplot as plt
 
 from attacker import Attacker
-from CRPTransform import NoTransform, XorKeyTransform, TFFWithResetTransform, ElipticCurveTransform
+from CRPTransform import NoTransform, XorKeyTransform, DoubleXorTransform, TFFWithResetTransform, ElipticCurveTransform
 
-CRPNUMS = [100, 1000, 10000, 50000, 100000, 250000, 500000]
+CRPNUMS = [100, 1000, 10000, 50000, 75000, 100000, 250000, 500000]
 
 def main():
     notf_accuracies = []
@@ -19,6 +19,12 @@ def main():
         xor_key_attacker = Attacker(XorKeyTransform, key=0xfedcba0987654321, N=N, n=64, k=5)
         xor_key_attacker.fit_attack()
         xor_accuracies.append(xor_key_attacker.attack_accuracy())
+
+    double_xor_accuracies = []
+    for N in CRPNUMS:
+        double_xor_attacker = Attacker(DoubleXorTransform, key=0xfedcba0987654321, N=N, n=64, k=5)
+        double_xor_attacker.fit_attack()
+        double_xor_accuracies.append(double_xor_attacker.attack_accuracy())
     
     tf_accuracies = []
     for N in CRPNUMS:
@@ -26,20 +32,22 @@ def main():
         tff_attacker.fit_attack()
         tf_accuracies.append(tff_attacker.attack_accuracy())
 
-    plot_accuracies(notf_accuracies, xor_accuracies, tf_accuracies)
+    plot_accuracies(notf_accuracies, xor_accuracies, double_xor_accuracies, tf_accuracies)
 
     notf_entropy = notf_attacker.transform_entropy()
     xor_entropy = xor_key_attacker.transform_entropy()
+    double_xor_entropy = double_xor_attacker.transform_entropy()
     tf_entropy = tff_attacker.transform_entropy()
 
-    plot_entropies(notf_entropy, xor_entropy, tf_entropy)
+    plot_entropies(notf_entropy, xor_entropy, double_xor_entropy, tf_entropy)
 
     print("TODO: ELIPTIC CURVE ATTACKS")
 
-def plot_accuracies(notf_accuracies, xor_accuracies, tf_accuracies):
-    print("accuracies:", notf_accuracies, xor_accuracies, tf_accuracies)
+def plot_accuracies(notf_accuracies, xor_accuracies, double_xor_accuracies, tf_accuracies):
+    print("accuracies:", notf_accuracies, xor_accuracies, double_xor_accuracies, tf_accuracies)
     plt.plot(notf_accuracies, label="No Transform")
     plt.plot(xor_accuracies, label="Xor Key")
+    plt.plot(double_xor_accuracies, label="Double Xor")
     plt.plot(tf_accuracies, label="TFF With Reset")
     plt.xticks(list(range(len(CRPNUMS))), CRPNUMS)
     plt.xlabel("Number of CRPs")
@@ -48,10 +56,11 @@ def plot_accuracies(notf_accuracies, xor_accuracies, tf_accuracies):
     plt.savefig("figs/accuracies.png")
     plt.clf()
 
-def plot_entropies(notf_entropy, xor_entropy, tf_entropy):
-    print("entropies:", notf_entropy, xor_entropy, tf_entropy)
-    plt.bar(["No Transform", "Xor Key", "TFF With Reset"], [notf_entropy, xor_entropy, tf_entropy])
-    plt.legend()
+def plot_entropies(notf_entropy, xor_entropy, double_xor_entropy, tf_entropy):
+    print("entropies:", notf_entropy, xor_entropy, double_xor_entropy, tf_entropy)
+    plt.bar(["No Transform", "Xor Key", "Double Xor", "TFF With Reset"], 
+            [notf_entropy, xor_entropy, double_xor_entropy, tf_entropy])
+    plt.ylabel("Entropy")
     plt.savefig("figs/entropies.png")
     plt.clf()
 
